bc41738261a587f729bb3cdc5c89b4e24ec6c862
TriUbi <triubilla@gmail.com>
2024-02-07 11:51:30 +0100

    scss styles till root

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,3 +1,12 @@
+import {Outlet} from "react-router-dom"
+import "./../styles/main.scss";
+
 export const Layout=()=>{
-    return <>Layout</>
+    return <>
+    <header></header>
+    <main>
+        <Outlet/>
+    </main>
+    <footer></footer>
+    </>
 };
