b73134f6bc396e4b88f5b289a0982388b486fd71
TriUbi <triubilla@gmail.com>
2024-02-08 23:29:42 +0100

    localStorage done

--- src/pages/MovieSearchApp.tsx
+++ src/pages/MovieSearchApp.tsx
@@ -6,11 +6,12 @@ import axios from "axios";
 import { SearchResult } from "../components/SearchResult";
 
 export const MovieSearchApp=()=>{
- const [movies, setMovies] = useState<IMovie[]>();
+const [movies, setMovies] = useState<IMovie[]>(JSON.parse(localStorage.getItem("movies") || "[]"));
 
  const searchMovies = async (searchText: string) => {
    const response = await axios.get<IOmdbResponse>( "https://omdbapi.com/?apikey=416ed51a&s=" + searchText)
    setMovies(response.data.Search); 
+   localStorage.setItem("movies", JSON.stringify(response.data.Search));
 
 }
 
